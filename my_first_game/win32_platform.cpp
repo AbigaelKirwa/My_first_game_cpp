e83a32998e95574d1d32726a5ce3d38275a50d63
Abykirwa1937 <55990700+Abykirwa1937@users.noreply.github.com>
2021-05-08 10:22:32 +0300

    Update win32_platform.cpp

--- my_first_game/win32_platform.cpp
+++ my_first_game/win32_platform.cpp
@@ -2,54 +2,47 @@
 
 bool running = true;
 
-LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	LRESULT result = 0;
-	switch (uMsg) 
-	{
-		case WM_CLOSE:
-		case WM_DESTROY: 
-		{
-			running = false;
-		} break;
 
-		default: 
-		{
-			result= DefWindowProc(hwnd, uMsg, wParam, lParam);
-		}
+	switch (uMsg) {
+	case WM_CLOSE:
+	case WM_DESTROY: {
+		running = false;
+	} break;
 
+	default: {
+		result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
 	}
 	return result;
 }
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
-{
-	//create window class
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	// Create Window Class
 	WNDCLASS window_class = {};
 	window_class.style = CS_HREDRAW | CS_VREDRAW;
 	window_class.lpszClassName = "Game Window Class";
 	window_class.lpfnWndProc = window_callback;
 
-
-	//register window
+	// Register Class
 	RegisterClass(&window_class);
 
-	//create window
-	HWND window = CreateWindow(window_class.lpszClassName, "My first Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	// Create Window
+	HWND window = CreateWindow(window_class.lpszClassName, "My First Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
 
-	while (running) 
-	{
-		//input
+	while (running) {
+		// Input
 		MSG message;
-		while (PeekMessage(&message, window, 0, 0, PM_REMOVE));
-		{
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&message);
 			DispatchMessage(&message);
 		}
 
-		//simulate
+		// Simulate
 
-		//render
-
+		// Render
 	}
+
 }
+
